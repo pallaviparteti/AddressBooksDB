6ea96475d6a3b3ae42216134b644b0aefa922d76
pallaviparteti <118168645+pallaviparteti@users.noreply.github.com>
2023-05-03 14:53:11 +0530

    Add: creating table in addressBook

--- AddressBookDB.sql
+++ AddressBookDB.sql
@@ -3,3 +3,10 @@ show databases;
 create database addressbooks_service;
 use addressbooks_service;
 show databases;
+-------------------------- UC2 -------------------------------------------------
+create table addressbook(firstName varchar(25) , 
+lastName varchar(25), address varchar(255), 
+city varchar(25), state varchar(25), 
+zip int, phoneNumber int, email varchar(255));
+show tables;
+desc addressbook;
